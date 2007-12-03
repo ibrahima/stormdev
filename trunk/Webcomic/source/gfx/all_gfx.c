e645cf45f9daadd7ef38fbbe51c51cc2aabc85ef
ibrahim.awwal <ibrahim.awwal@d8a144f9-5b40-0410-b64e-bff6e0f019e3>
2007-12-03 06:39:19 +0000

    Added a folder remotely

---
+++ trunk/Webcomic/source/gfx/all_gfx.c
@@ -0,0 +1,25 @@
+//Gfx converted using Mollusk's PAGfx Converter
+
+//This file contains all the .c, for easier inclusion in a project
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "all_gfx.h"
+
+
+// Background files : 
+#include "menu.c"
+
+// Palette files : 
+#include "menu.pal.c"
+
+// Background Pointers :
+PAGfx_struct menu = {(void*)menu_Map, 768, (void*)menu_Tiles, 31616, (void*)menu_Pal, (int*)menu_Info };
+
+
+#ifdef __cplusplus
+}
+#endif
+
